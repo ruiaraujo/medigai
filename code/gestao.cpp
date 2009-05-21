9d6c091cd37d269b1833f858b3d0282c18b6bc3b
bruno.ff.brito <bruno.ff.brito@6a8f129e-1565-11de-989c-57ac4e9a12f8>
2009-05-21 17:54:38 +0000

    menu

--- code/gestao.cpp
+++ code/gestao.cpp
@@ -1,1 +1,182 @@
+#include "classes/headers/MemoryHandling.h"
+#include <vector>
+#include <cstdlib>
+#include<iostream>
 
+using namespace std;
+
+string file_med = "medico.txt",file_pac="utentes.txt",file_esp="especialidade.txt"; //nome dos ficheiros para leitura
+vector<Medico *> lista_med;
+vector<Utente *> lista_pac;
+vector<Especialidade *> lista_esp;
+
+void log();
+void menupr();
+void menu_med();
+
+void menu_consulta(){
+cout<<"Menu:\n\nInsira o numero da opção desejada.\n\n1. Inserir Consulta\n2. Alterar Consulta\n3.Visualizar Consulta\n4. Visualizar Horario\n\n";
+
+char opcao;
+  
+  while ( ! cin.eof() ) 
+  {
+    cin >> opcao;
+    if ( ! cin.eof() && isdigit( opcao ) )
+    {
+      switch (opcao)
+      {
+        case '1': cout << "Insira os Dados da Consulta.\n";
+                  //insCon( lista_med , lista_pac , lista_con );
+                  //listar ( lista_con );
+                  menu_consulta();
+                  cin.clear();
+                  break;
+        case '2': cout << "Insira os dados da Consulta:" << endl;
+                  //delCon( lista_med , lista_con );
+                  menu_consulta();
+                  cin.clear();
+                  break;        
+        case '3': cout << "Lista de Consultas:" << endl;
+                  //listar ( lista_con );
+                  menu_consulta();
+                  cin.clear();
+                  break;                  
+        default: cout << "\nOpção desconhecida.\n\n";
+                 menu_consulta();
+                 cin.clear();
+                 break;
+      }
+    }
+  }
+}
+
+
+int main(){
+
+if ( criaFile( file_med ) )
+    fstream med( file_med.c_str() );
+  else
+  {
+    cerr << "Erro: Verifique permissões no directório de execução."<<endl;
+    return -1;
+  }
+  if ( criaFile( file_esp) )
+    fstream med( file_esp.c_str() );
+  else
+  {
+    cerr << "Erro: Verifique permissões no directório de execução."<<endl;
+    return -1;
+  }
+  if ( criaFile( file_pac ) )
+    fstream med( file_pac.c_str() );
+  else
+  {
+    cerr << "Erro: Verifique permissões no directório de execução."<<endl;
+    return -1;
+  }
+
+menupr();
+return 0;
+
+}
+
+void log(){
+
+cout<<"******************************************************************************"<<endl;
+cout<<"*    __          __   ____   ________  ( )  _______        ______       ( )  *"<<endl;
+cout<<"*   |  \\        /  | |____| |  __    |  _  |  _____|      /  __  \\       _   *"<<endl;
+cout<<"*   |  |\\      /|  | |____  | |  |   | | | | |           /  /  \\  \\     | |  *"<<endl;
+cout<<"*   |  | \\    / |  | |____| | |  |   | | | | |_____     /  /____\\  \\    | |  *"<<endl;
+cout<<"*   |  |  \\  /  |  | |____  | |__|   | | | | |_____|   /  /______\\  \\   | |  *"<<endl;
+cout<<"*   |__|   \\/   |__| |____| |________| |_| |_______|  /__/        \\__\\  |_|  *"<<endl;
+cout<<"*                                                                            *"<<endl; 
+cout<<"*                 Medical Management for Linux Lovers.                       *"<<endl;
+cout<<"*                                                                            *"<<endl;
+cout<<"******************************************************************************"<<endl;
+
+}
+
+void menupr(){
+system("clear");
+log();
+
+cout << "Menu:\n\nInsira o número da opção desejada.\n\n1. Corpo Clínico\n2. Gestão de Utentes\n";
+cout << "3. Gestão de Consultas\n4. Guardar as alterações\n";
+cout << "Prima 'X' ou 'x' para sair.\n";
+
+char opcao;
+
+
+while(!cin.eof()){
+
+cin>>opcao;
+if (!cin.eof() && isdigit(opcao))
+{
+switch (opcao)
+      {
+        case '1': menu_med();
+                  break;
+        case '2': //menu_utente();
+		  break;
+        case '3': menu_consulta();
+		  break;
+	case '4': //savPac ( file_pac , lista_pac );
+                  //savMed ( file_med , lista_med );
+                  //savEsp ( file_esp , lista_esp );
+                  //carregaEsp( file_esp , lista_esp );
+                  //carregaMed( file_med , lista_med , lista_esp );
+                  //listar( lista_med );
+                  cout << endl;
+                  //listar( lista_esp );
+                  //carregaPac ( file_pac , lista_pac );
+                  cout << endl;
+                  //listar( lista_pac );
+                  cout <<"Alterações guardadas."<< endl;
+		  menupr();
+			
+	case 'x': break;
+	case 'X': break;
+        default: cout << "\nOpção desconhecida.\n\n";
+		 break;
+	
+
+	}
+}}}
+
+void menu_med(){
+system("clear");
+log();
+char op;
+cout << "\n\nGestão do Corpo Clínico:\n\nInsira o número da opção desejada.\n\n";
+  cout << "1. Adicionar Médico\n2. Eliminar Médico\n3. Visualisar os médicos\n";
+  cout << "CRTL-D para voltar ao menu anterior.\n";
+cin>>op;
+if ( ! cin.eof() && isdigit( op ) )
+    {
+switch(op){
+
+case '1': cout << "Insira os Dados do Médico.\n";
+                  //insMed( lista_med , lista_esp );
+                  //listar( lista_med );
+                  menu_med();
+                  cin.clear();
+                  break;
+case '2': cout << "Insira a cédula do médico.\n";
+                  long cedula;
+                  cin >> cedula;
+                  //delMed ( cedula , lista_med );
+                  menu_med();
+                  cin.clear();
+                  break;
+case '3': //listar ( lista_med );
+                  cout << endl;
+	  menu_med();
+	  break;
+
+default: cout << "\nOpção desconhecida.\n\n";
+		 break;
+	}}
+}
+
+ 
